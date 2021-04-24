29ca6554f9487287558f05e33ac45b4d0c63b1d7
Bengemon825 <62958342+Bengemon825@users.noreply.github.com>
2021-04-24 18:28:18 -0400

    messing with search algorithms

--- search.py
+++ search.py
@@ -34,12 +34,42 @@ def tinyMazeSearch(problem):
 # -------------------------------------- Gonna need to change things sorry buddy
 # def main():
 #     # main can call the different search functions (ideally we should set up command line args but idk how to do that so)
-#     # depthFirstSearch()
+#     depthFirstSearch()
 #     return
 #
 #
 # def depthFirstSearch():
 #     print("Starting depth first search algorithm...")
+#     from util import Stack
+#     import snakeOLD
+#     from snakeOLD import snake
+#
+#     snek = snake
+#     s = snakeOLD
+#     dfs_stack = Stack()  # fringe
+#     visited = set()
+#     dfs_stack.push((snek.getStartState(), []))
+#
+#     while 1:
+#         if dfs_stack.isEmpty():
+#             print("Failure")
+#             break
+#         current, directions = dfs_stack.pop()  # popping the directions with the nodes gives optimal directions
+#         # print("Current:", current)
+#         if current not in visited:
+#             visited.add(current)
+#             if snek.isGoalState(current):
+#                 print("Success")
+#                 print(directions)
+#                 return directions
+#
+#             for childNode, direction, cost in snek.getSuccessors(current):
+#                 if childNode not in dfs_stack.list:
+#                     if childNode in visited:  # make sure child is not in visited so we don't go backwards
+#                         continue
+#                     # print(direction)
+#                     # print(childNode, direction)
+#                     dfs_stack.push((childNode, directions + [direction]))
 #     return
 #
 #
